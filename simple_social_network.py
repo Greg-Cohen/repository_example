b526a363bd3444ab829785cc38a2ff17e113213a
Greg-Cohen <gcohen31@terpmail.umd.edu>
2022-03-29 13:15:42 -0400

    added notes at the top

--- simple_social_network.py
+++ simple_social_network.py
@@ -1,3 +1,11 @@
+# To get script from working directory to the local repository:
+    # Add to the staging area for commiting, then commit it to the local repository:
+        # run command: git add (insert file name here) and press Enter   <-- add it to the staging area
+        # run command: git commit -m "(insert what was done to let partners know what was changed)"   <-- this will commit it to the local repository
+# To share changes from local repository to partners:
+    # run command: git push  <-- pushes to the remote repository where other people can work with it
+        
+
 """ A simple social network for tracking connections between people example """
 
 class Person:
